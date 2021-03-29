af160b3d4352b003ff84963f729bdeee1d73d858
yumi <dldbal123@google.com>
2021-03-28 21:40:37 -0700

    회원정보 화면

--- app/src/main/java/com/example/sharingbookshelf/Activities/RegisterInforActivity.java
+++ app/src/main/java/com/example/sharingbookshelf/Activities/RegisterInforActivity.java
@@ -6,33 +6,37 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sharingbookshelf.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterInforActivity extends Activity {
 
-    Button btn_age, btn_camera;
+    Button btn_camera, btn_address;
+    Spinner sp_age;
+    TextView homeview, et_address;
+    String[] ages = {"1세","2세","3세","4세","5세","6세","7세","8세","9세","10세",
+            "11세","12세","13세","14세","15세","16세","17세","18세"};
+
+    private static final int SEARCH_ADDRESS_ACTIVITY = 10000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reginfor);
-        btn_age = (Button)findViewById(R.id.reg_childage);
-        btn_age.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                showDialog(1);
-            }
-        });
 
         // 카메라로 전환
-        btn_camera = (Button)findViewById(R.id.camera_button);
+        btn_camera = (Button) findViewById(R.id.camera_button);
         btn_camera.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), TakingPhotoActivity.class); //1번 파라미터 : 메인 액티비티 자신, 2번 : 호출할 클래스
@@ -41,38 +45,61 @@ public class RegisterInforActivity extends Activity {
         });
 
         // 홈 화면으로 전환
-    }
-
-    // 자녀 나이 버튼 선택하면 선택하는 팝업
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        // TODO Auto-generated method stub
-        final String [] items = {"사과", "딸기", "참외", "바나나", "두리안"};
-        AlertDialog.Builder builder = new AlertDialog.Builder(RegisterInforActivity.this);
-        builder.setTitle("자녀 나이를 선택하세요");
-        builder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // TODO Auto-generated method stub
-                Toast.makeText(RegisterInforActivity.this, items[which], Toast.LENGTH_SHORT).show();
-                dialog.dismiss(); // 누르면 바로 닫히는 형태
+        homeview = (TextView) findViewById(R.id.homeView);
+        homeview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class); //1번 파라미터 : 메인 액티비티 자신, 2번 : 호출할 클래스
+                startActivity(intent); //intent => Activity끼리 서로 호출하기 위해서 필요한 통신장치.
             }
         });
-        return builder.create();
+
+        // 자녀 나이 선택
+        sp_age = (Spinner) findViewById(R.id.reg_childAge);
+        //age = findViewById(R.id.showage);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, ages);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_age.setAdapter(adapter);
+
+//        sp_age.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            // 선택된 상태
+//           @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                text.setText(ages[position]);
+//            }
+//            // 선택되지 않은 상태
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//                text.setText("자녀 나이를 입력하세요");
+//            }
+//        });
+
+        // 주소지 설정 팝업
+        btn_address = findViewById(R.id.reg_address);
+
+        if (btn_address != null) {
+            btn_address.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(RegisterInforActivity.this, WebViewActivity.class);
+                    startActivityForResult(i, SEARCH_ADDRESS_ACTIVITY);
+                }
+            });
+        }
     }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        super.onActivityResult(requestCode, resultCode, intent);
+        switch (requestCode) {
+            case SEARCH_ADDRESS_ACTIVITY:
+                if (resultCode == RESULT_OK) {
+                    String data = intent.getExtras().getString("data");
+                    if (data != null) {
+                        et_address.setText(data);
+                    }
+                }
+                break;
+        }
+    }
 
 }
-
-
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_reginfor);
-//    }
-
-    // 최초 로그인 후 register infor 화면으로 이동
-
-//}
