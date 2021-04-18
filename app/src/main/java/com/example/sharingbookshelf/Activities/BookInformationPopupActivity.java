2c2990203daaa2a020ee5269ea3d77b4bca10e38
yumi <79792066+yuuum1@users.noreply.github.com>
2021-04-18 18:10:46 +0900

    Feature/Book-Information-Popup (#25)
    * fix menu bar image
    
    * design book information layout
    
    * add sharing state
    
    * fix sharing state
    
    * book Information Popup
    
    Co-authored-by: yumi <dldbal123@google.com>

--- app/src/main/java/com/example/sharingbookshelf/Activities/BookInformationPopupActivity.java
+++ app/src/main/java/com/example/sharingbookshelf/Activities/BookInformationPopupActivity.java
@@ -3,30 +3,60 @@ package com.example.sharingbookshelf.Activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.example.sharingbookshelf.HttpRequest.RetrofitServiceApi;
+import com.bumptech.glide.Glide;
+import com.example.sharingbookshelf.Models.BookApiResponse;
 import com.example.sharingbookshelf.R;
+
+import java.util.List;
 
 public class BookInformationPopupActivity extends Activity {
 
+    private ImageView iv_thumbNail;
+    private TextView tv_title, tv_authors, tv_publisher, tv_sharingstate;
     private EditText BookMemo_field;
-    private RetrofitServiceApi retrofitServiceApi;
+    private Button btn_addBook, btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //타이틀바 없애기
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //타이틀바 없애기
         setContentView(R.layout.activity_book_information_popup);
 
         //UI 객체생성
         BookMemo_field = (EditText)findViewById(R.id.BookMemo_field);
 
     }
-    // 뒤로 가기
 
-    // 메모 입력
+    private void initializeView() {
+        iv_thumbNail = findViewById(R.id.iv_thumbnail);
+        tv_title = findViewById(R.id.tv_title);
+        tv_authors = findViewById(R.id.tv_authors);
+        tv_publisher = findViewById(R.id.tv_publisher);
+        tv_sharingstate = findViewById(R.id.tv_sharingstate);
+        btn_back = findViewById(R.id.btn_back);
+    }
 
-    // 메모 등
+    // 썸네일, 제목, 저자, 출판사, 정가 불러오기
+    private void setView(BookApiResponse.Document book) {
+        String isbn = book.getIsbn();
+        String title = book.getTitle();
+        List<String> authors = book.getAuthors();
+        String publisher = book.getPublisher();
+        String thumbnail = book.getThumbnail();
+        Glide.with(this).load(thumbnail).into(iv_thumbNail);
+
+        tv_title.setText(title);
+        tv_authors.setText(authors.get(0));
+        tv_publisher.setText(publisher);
+    }
+
+    // 메모 등록
+
+    // 판매중, 공유중, 공유불가 상태
+
 }
