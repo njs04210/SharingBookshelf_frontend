a06f56329d29afc56d1fa4feea547fc5748d2530
songseryeon <thdtpfus@nate.com>
2021-04-24 19:18:25 +0900

    final commit

--- app/src/main/java/com/example/sharingbookshelf/Activities/ProgressDialogActivity.java
+++ app/src/main/java/com/example/sharingbookshelf/Activities/ProgressDialogActivity.java
@@ -13,20 +13,20 @@ public class ProgressDialogActivity{
     public static void showLoading(Context context) {
 
         if (m_loadingDialog == null) {
-            //다이얼로그가 없으면 만들고 보이게 하라
-            m_loadingDialog = new Dialog(context, R.style.layout_progressbar);
-            //프로그레스를 생성하자
+            //다이얼로그가 없으면 만들고 보이게하기
+            m_loadingDialog = new Dialog(context, R.style.custom_loading);
+            //프로그레스를 생성
             ProgressBar pb = new ProgressBar(context);
             LayoutParams params = new LayoutParams(
                     LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-            //프로그래스를 다이얼로그에 포함하자
+            //프로그레스를 다이얼로그에 포함
             m_loadingDialog.setContentView(pb, params);
             m_loadingDialog.setCancelable(false);
             m_loadingDialog.show();
         } else if(!m_loadingDialog.isShowing()){
-            //다이얼로그가 있는데 HIDE 상태면 보이게 하라
+            //다이얼로그가 있는데 HIDE 상태면 보이게
             m_loadingDialog = null;
-            m_loadingDialog = new Dialog(context, R.style.layout_progressbar);
+            m_loadingDialog = new Dialog(context, R.style.custom_loading);
             ProgressBar pb = new ProgressBar(context);
             LayoutParams params = new LayoutParams(
                     LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
@@ -39,7 +39,7 @@ public class ProgressDialogActivity{
     public static void hideLoading() {
         if (m_loadingDialog != null) {
             if(m_loadingDialog.isShowing()){
-                //다이얼로그가 있고 보이는 상태면 안보이게 하라
+                //다이얼로그가 있고 보이는 상태면 안보이게
                 m_loadingDialog.dismiss();
                 m_loadingDialog = null;
             }
