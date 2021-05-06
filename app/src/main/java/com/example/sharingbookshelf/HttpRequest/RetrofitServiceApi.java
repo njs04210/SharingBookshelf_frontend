948c265b2c0323619149ddb14cbdfe0c699e9f15
KimJinJoo <pearlnkim@gmail.com>
2021-05-06 13:50:34 +0900

    Feature/Retrofit-myBookshelf (#32)
    * new method : setShelfView
    
    * add Book to Server
    
    * change server address
    
    * change server address
    
    * change name of parameter
    
    * change code
    
    * modify layout - activity_bookinfo_popup

--- app/src/main/java/com/example/sharingbookshelf/HttpRequest/RetrofitServiceApi.java
+++ app/src/main/java/com/example/sharingbookshelf/HttpRequest/RetrofitServiceApi.java
@@ -1,23 +1,25 @@
 package com.example.sharingbookshelf.HttpRequest;
 
+import com.example.sharingbookshelf.Models.AddBookResponse;
 import com.example.sharingbookshelf.Models.BookApiResponse;
+import com.example.sharingbookshelf.Models.CreateShelfResponse;
+import com.example.sharingbookshelf.Models.GetShelfStatusResponse;
 import com.example.sharingbookshelf.Models.GetUserInfoResponse;
 import com.example.sharingbookshelf.Models.LoginResponse;
 import com.example.sharingbookshelf.Models.SetUserInfoResponse;
 import com.example.sharingbookshelf.Models.UserInfoData;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
-import retrofit2.http.QueryName;
-import retrofit2.http.Url;
 
 //DAO랑 비슷
 //Call안에 서버로부터 넘겨받을 데이터 구조 정의
@@ -34,6 +36,16 @@ public interface RetrofitServiceApi {
 
     @GET("api/users/{memId}")
     Call<GetUserInfoResponse> getUserInfo(@Path("memId") int memId);
+
+    @GET("api/bookshelves/{memId}")
+    Call<GetShelfStatusResponse> getShelfStatus(@Path("memId") int memId);
+
+    @POST("api/bookshelves")
+    Call<CreateShelfResponse> createShelf();
+
+    @FormUrlEncoded
+    @POST("api/bookshelves/{memId}")
+    Call<AddBookResponse> addBookInShelf(@Path("memId") int memId, @FieldMap HashMap<String, Object> parameters);
 
 }
 
