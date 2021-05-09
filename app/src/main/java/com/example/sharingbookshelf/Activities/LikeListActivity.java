558dccca8153b70e0ebc89267ea59aacf911952e
njs04210 <pearlnkim@gmail.com>
2021-05-09 18:40:59 +0900

    refactoring

--- app/src/main/java/com/example/sharingbookshelf/Activities/LikeListActivity.java
+++ app/src/main/java/com/example/sharingbookshelf/Activities/LikeListActivity.java
@@ -3,24 +3,16 @@ package com.example.sharingbookshelf.Activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
-import com.example.sharingbookshelf.Models.BookApiResponse;
 import com.example.sharingbookshelf.R;
 
-import java.util.List;
-
-public class EditProfileActivity extends Activity {
+public class LikeListActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.layout_edit_profile);
+        setContentView(R.layout.activity_like_list);
 
     }
 
--- app/src/main/java/com/example/sharingbookshelf/Fragments/MyPageFragment.java
+++ app/src/main/java/com/example/sharingbookshelf/Fragments/MyPageFragment.java
@@ -1,5 +1,6 @@
 package com.example.sharingbookshelf.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,10 +9,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.sharingbookshelf.Activities.EditProfileActivity;
+import com.example.sharingbookshelf.Activities.LeaveIdActivity;
+import com.example.sharingbookshelf.Activities.LikeListActivity;
+import com.example.sharingbookshelf.Activities.MainActivity;
 import com.example.sharingbookshelf.R;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class MyPageFragment extends Fragment {
+public class MyPageFragment extends Fragment implements View.OnClickListener {
+
+
+    private FirebaseAuth mAuth;
 
     private Button btn_editProfile;
     private Button btn_dealList;
@@ -33,14 +43,65 @@ public class MyPageFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_my_page, container, false);
 
         btn_editProfile = v.findViewById(R.id.btn_editProfile);
-        btn_dealList = v.findViewById(R.id.btn_dealList);
-        btn_likeList = v.findViewById(R.id.btn_likeList);
-        btn_memoList = v.findViewById(R.id.btn_memoList);
-        btn_logout = v.findViewById(R.id.btn_logout);
-        btn_leaveId = v.findViewById(R.id.btn_leaveId);
+        btn_editProfile.setOnClickListener(this);
 
+        btn_dealList = v.findViewById(R.id.btn_dealList);
+        btn_dealList.setOnClickListener(this);
+
+        btn_likeList = v.findViewById(R.id.btn_likeList);
+        btn_likeList.setOnClickListener(this);
+
+        btn_memoList = v.findViewById(R.id.btn_memoList);
+        btn_memoList.setOnClickListener(this);
+
+        btn_logout = v.findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(this);
+
+        btn_leaveId = v.findViewById(R.id.btn_leaveId);
+        btn_leaveId.setOnClickListener(this);
 
         return v;
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.btn_editProfile:
+                intent = new Intent(getActivity(), EditProfileActivity.class);
+                startActivity(intent);
+                getActivity().overridePendingTransition(R.anim.anim_slide_in_bottom, R.anim.anim_slide_out_top);
+                break;
+
+            case R.id.btn_dealList:
+                //추가 예정
+                Toast.makeText(getActivity().getApplicationContext(), "거래목록", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btn_likeList:
+                intent = new Intent(getActivity(), LikeListActivity.class);
+                startActivity(intent);
+                getActivity().overridePendingTransition(R.anim.anim_slide_in_bottom, R.anim.anim_slide_out_top);
+                break;
+
+            case R.id.btn_memoList:
+                //추가 예정
+                Toast.makeText(getActivity().getApplicationContext(), "메모목록", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btn_logout:
+                //추가 예정
+                Toast.makeText(getActivity().getApplicationContext(), "로그아웃", Toast.LENGTH_SHORT).show();
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity(), MainActivity.class));
+                break;
+
+            case R.id.btn_leaveId:
+                intent = new Intent(getActivity(), LeaveIdActivity.class);
+                startActivity(intent);
+                getActivity().overridePendingTransition(R.anim.anim_slide_in_bottom, R.anim.anim_slide_out_top);
+                break;
+        }
     }
 }
