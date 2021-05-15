d6c725cab44201c14f5148bd4a7789473accd503
kangjisoo <kjsoo0402@naver.com>
2021-05-15 13:27:49 +0900

    layout_create_book_report

--- app/src/main/java/com/example/sharingbookshelf/Fragments/BookReportFragment.java
+++ app/src/main/java/com/example/sharingbookshelf/Fragments/BookReportFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.sharingbookshelf.R;
 
 public class BookReportFragment extends Fragment {
+
+    private Button btn_write;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,16 @@ public class BookReportFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_book_report, container, false);
+
+         btn_write = v.findViewById(R.id.btn_noForm);
+
+         btn_write.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 BookReportFragment2 e = new BookReportFragment2();
+                 e.show(getActivity().getSupportFragmentManager(), "Abc");
+             }
+         });
 
         return v;
 
