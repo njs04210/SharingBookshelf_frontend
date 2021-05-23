cd946805e746c4b32a825fadba3defcbcd2da349
njs04210 <pearlnkim@gmail.com>
2021-05-24 01:32:25 +0900

    refactoring

--- app/src/main/java/com/example/sharingbookshelf/Fragments/AccessWarningFragment.java
+++ app/src/main/java/com/example/sharingbookshelf/Fragments/AccessWarningFragment.java
@@ -23,7 +23,6 @@ public class AccessWarningFragment extends DialogFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setStyle(STYLE_NORMAL, R.style.CustomBottomSheetDialogTheme);
     }
 
