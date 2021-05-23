39c2c826e7ae6a29f6392e5485dc04f2e2da256f
KimJinJoo <pearlnkim@gmail.com>
2021-05-23 13:08:58 +0900

    Feature/BookReportBox (#44)
    * layout_create_book_report
    
    * book_report_list
    
    * Refactoring
    
    Co-authored-by: kangjisoo <kjsoo0402@naver.com>

---
+++ app/src/main/java/com/example/sharingbookshelf/Models/BookreportData.java
@@ -0,0 +1,31 @@
+package com.example.sharingbookshelf.Models;
+
+public class BookreportData {
+    int painting;
+    String booktitle;
+    String date;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getBooktitle() {
+        return booktitle;
+    }
+
+    public void setBooktitle(String booktitle) {
+        this.booktitle = booktitle;
+    }
+
+    public int getPainting() {
+        return painting;
+    }
+
+    public void setPainting(int painting) {
+        this.painting = painting;
+    }
+}
