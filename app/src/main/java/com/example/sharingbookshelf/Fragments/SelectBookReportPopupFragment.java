566f5ca59281cc4ceb6275c9eed0028f5a49bd1b
njs04210 <pearlnkim@gmail.com>
2021-05-26 02:22:18 +0900

    Fragment - Select BookReport Popup

--- app/src/main/java/com/example/sharingbookshelf/Fragments/SelectBookReportPopupFragment.java
+++ app/src/main/java/com/example/sharingbookshelf/Fragments/SelectBookReportPopupFragment.java
@@ -3,6 +3,7 @@ package com.example.sharingbookshelf.Fragments;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -15,14 +16,26 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.sharingbookshelf.Activities.CreateBookReportActivity;
+import com.example.sharingbookshelf.Adapters.SelectBookreportAdapter;
+import com.example.sharingbookshelf.Models.SelectReportData;
+import com.example.sharingbookshelf.Models.MessageData;
 import com.example.sharingbookshelf.R;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class SelectBookReportPopupFragment extends DialogFragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.Adapter mAdapter;
+    private ArrayList<SelectReportData> selectbookList = new ArrayList<>();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,6 +47,16 @@ public class SelectBookReportPopupFragment extends DialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_select_book_report_popup, container, false);
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.rv_selectbookreport);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        setTitlelist();
+
+        mAdapter = new SelectBookreportAdapter(selectbookList);
+        mRecyclerView.setAdapter(mAdapter);
 
         Button btn_confirm = v.findViewById(R.id.btn_confirm);
 
@@ -47,6 +70,15 @@ public class SelectBookReportPopupFragment extends DialogFragment {
             }
         });
         return v;
+    }
+
+    private void setTitlelist() {
+        for (int i = 0; i < 10; i++) {
+            SelectReportData selectreportData = new SelectReportData();
+            selectreportData.setBooktitle("얼마나 길어도 계속 나올까요오옹");
+
+            selectbookList.add(selectreportData);
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/example/sharingbookshelf/Models/SelectReportData.java
@@ -0,0 +1,14 @@
+package com.example.sharingbookshelf.Models;
+
+public class SelectReportData {
+
+    String booktitle;
+
+    public String getBooktitle() {
+        return booktitle;
+    }
+
+    public void setBooktitle(String booktitle) {
+        this.booktitle = booktitle;
+    }
+}
