bef9b05b8041813df4e219a53b6354eb4535e7fa
njs04210 <pearlnkim@gmail.com>
2021-05-29 02:46:30 +0900

    modify layout : FilterCategoryFragment

--- app/src/main/java/com/example/sharingbookshelf/Fragments/FilterCategoryFragment.java
+++ app/src/main/java/com/example/sharingbookshelf/Fragments/FilterCategoryFragment.java
@@ -1,15 +1,10 @@
 package com.example.sharingbookshelf.Fragments;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.WindowMetrics;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -43,8 +38,6 @@ public class FilterCategoryFragment extends DialogFragment {
         Chip fairyChip = v.findViewById(R.id.chip_fairy_f);
         Chip cartoonChip = v.findViewById(R.id.chip_cartoon_f);
         Chip etcChip = v.findViewById(R.id.chip_etc_f);
-
-
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
